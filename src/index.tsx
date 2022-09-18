22ccbf5adb56abacad454c4e0c99181fa7c212b2
GurevichV <melnikova.viki@yandex.ru>
2022-09-18 14:59:25 +0300

    added hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <Provider store={store}>
           <App />
         </Provider>
       </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
 );
