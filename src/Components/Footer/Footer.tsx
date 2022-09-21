411b16c205d1fad70b45db8563a083a0368a073e
GurevichV <melnikova.viki@yandex.ru>
2022-09-21 15:38:06 +0300

    DS-13 Added footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setUserInfo } from '../../actions';
+
+import { BsPersonFill  } from 'react-icons/bs';
+import { FaQuestion, FaSearch  } from 'react-icons/fa';
+import { MdLogin  } from 'react-icons/md';
+
 
 import './Footer.sass'
 
 const Footer = () => {
+
+    const {user_info} = useSelector(state => state) as any;
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+    const user_info = JSON.parse(localStorage.getItem('user-info'))
+    dispatch(setUserInfo(user_info))
+    }, []);
+
+
     return(
         <footer className="footer">
-            <div>Adress</div>
-            <div>Social</div>
-            <div>Menu</div>
+            <div><Link to="/search"><FaSearch /></Link></div>
+            <div><Link to="/faq"><FaQuestion/></Link></div>
+            { Object.keys(user_info).length === 0 ? <div><Link to="/signin"><MdLogin/></Link></div> : <div><Link to="/profile"><BsPersonFill/></Link></div>}
         </footer>
     )
 
+
+    
 }
 
 export default Footer;
